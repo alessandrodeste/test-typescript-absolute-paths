4b9fd5198bfe8b78e5c3f7283d3f64773f3ecc91
Alessandro d'Este <alessandrodeste@gmail.com>
2018-09-25 10:46:20 +0100

    new component with relative path

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 
 import logo from './logo.svg';
 
+import MyComp from './MyComp';
+
 class App extends React.Component {
   public render() {
     return (
@@ -14,6 +16,8 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <MyComp />
       </div>
     );
   }
---
+++ src/MyComp.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const MyComp = () => <div>MyComp</div>;
+
+export default MyComp;
